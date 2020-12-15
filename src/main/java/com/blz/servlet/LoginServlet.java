2301daf04e4cc3cef4afa2b37e7e89732c3a628c
Your Name <you@example.com>
2020-12-15 21:01:19 +0530

    Added: code to validate password

--- src/main/java/com/blz/servlet/LoginServlet.java
+++ src/main/java/com/blz/servlet/LoginServlet.java
@@ -13,10 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(description = "Login Servlet Testing", urlPatterns = { "/LoginServlet" }, initParams = {
-		@WebInitParam(name = "user", value = "Sreekar"), @WebInitParam(name = "password", value = "Login") })
+		@WebInitParam(name = "user", value = "Sreekar"), @WebInitParam(name = "password", value = "$Sree1998@") })
 
 public class LoginServlet extends HttpServlet {
 	private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}$";
+	private static final String PASSWORD_PATTERN = "^(?=.*[@#$%^&+=])(?=.*[0-9])(?=.*[A-Z]).{8,}$";
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -25,11 +26,13 @@ public class LoginServlet extends HttpServlet {
 		boolean validateFirstName = validateFirstName(user);
 		boolean checkFirstName = checkFirstName(request, response, validateFirstName);
 		String pwd = request.getParameter("pwd");
-
+		boolean validatePassword = validatePassword(pwd);
+		boolean checkPassword = checkPassword(request, response, validatePassword);
+		System.out.println("validatePassword " + validatePassword + " " + checkPassword);
 		String userID = getServletConfig().getInitParameter("user");
 		String password = getServletConfig().getInitParameter("password");
 
-		if (checkFirstName == true) {
+		if (checkFirstName == true && checkPassword == true) {
 			if (userID.equals(user) && password.equals(pwd)) {
 				request.setAttribute("user", user);
 				request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
@@ -54,9 +57,28 @@ public class LoginServlet extends HttpServlet {
 		return true;
 	}
 
+	private boolean checkPassword(HttpServletRequest request, HttpServletResponse response, boolean validatePassword)
+			throws IOException, ServletException {
+		if (validatePassword == false) {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red>Password is Incorrect</font>");
+			rd.include(request, response);
+			return false;
+		}
+		return true;
+	}
+
 	private boolean validateFirstName(String firstName) {
 		Pattern check = Pattern.compile(FIRST_NAME_PATTERN);
 		boolean value = check.matcher(firstName).matches();
 		return value;
 	}
+
+	private boolean validatePassword(String password) {
+		Pattern check = Pattern.compile(PASSWORD_PATTERN);
+		boolean value = check.matcher(password).matches();
+		return value;
+	}
+
 }
